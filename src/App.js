772a66bb5291da1999d1700059e5c5920c256d89
okantas <okantas954@gmail.com>
2023-12-27 00:56:26 +0300

    Landing background is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import SideBar from "./components/SideBar";
+import Landing from "./components/Landing";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <SideBar />
+      <Landing />
     </div>
   );
 }
---
+++ src/components/Landing.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Landing.css";
+
+export default function Landing() {
+  return <div className="landing-wrap"></div>;
+}
