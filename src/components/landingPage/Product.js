daff337d25e175d6b59e1ec2fb99baa7ed19ef7e
okantas <okantas954@gmail.com>
2024-01-14 00:26:59 +0300

    small watch svg's implemented

--- src/components/landingPage/Product.js
+++ src/components/landingPage/Product.js
@@ -1,5 +1,8 @@
 import React from "react";
 import BlueWatch from "./landing-icons/blueWatch.svg";
+import BlackWatch from "./landing-icons/blackWatch.svg";
+import GreenWatch from "./landing-icons/greenWatch.svg";
+import PinkWatch from "./landing-icons/pinkWatch.svg";
 import "./Product.css";
 
 export default function Product() {
@@ -15,6 +18,29 @@ export default function Product() {
       </div>
       <div className="watch">
         <img className="watch-img" src={BlueWatch} alt="blue apple watch " />
+        <div className="small-watches-wrapper">
+          <div className="small-watches">
+            <img
+              className="small-watches-img"
+              src={BlackWatch}
+              alt="small black apple watch "
+            />
+          </div>
+          <div className="small-watches">
+            <img
+              className="small-watches-img"
+              src={GreenWatch}
+              alt="small green apple watch"
+            />
+          </div>
+          <div className="small-watches">
+            <img
+              className="small-watches-img"
+              src={PinkWatch}
+              alt="small pink apple watch "
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
